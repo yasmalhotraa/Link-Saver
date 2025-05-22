bae3152bbf905bb1c0e2c7e1635e7d29a973a5a6
Yash Malhotra <yashmalhotra330@gmail.com>
2025-05-22 15:51:56 +0530

    added firebase to the application

---
+++ src/services/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAS5KMOLKMXWZLlny2CrXueif7iewH0K0U",
+  authDomain: "linksaver-46235.firebaseapp.com",
+  projectId: "linksaver-46235",
+  storageBucket: "linksaver-46235.firebasestorage.app",
+  messagingSenderId: "406696989108",
+  appId: "1:406696989108:web:018a74a660b6547d2c22e1",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { app, auth };
