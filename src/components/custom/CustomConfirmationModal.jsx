a4054311c913eea2dcaac4572acdf30ef5b6150d
Yash Malhotra <yashmalhotra330@gmail.com>
2025-05-23 18:24:45 +0530

    color theme correction

--- src/components/custom/CustomConfirmationModal.jsx
+++ src/components/custom/CustomConfirmationModal.jsx
@@ -3,20 +3,20 @@ import { Button } from "../ui/button";
 
 const CustomConfirmationModal = ({ message, onConfirm, onCancel }) => {
   return (
-    <div className="fixed inset-0 bg-gray-800 bg-opacity-50 flex justify-center items-center z-50 max-600:p-5">
-      <div className="bg-white p-6 rounded-md shadow-lg w-96">
+    <div className="fixed inset-0 bg-black/50 backdrop-blur-sm flex justify-center items-center z-50">
+      <div className="bg-[#f3eadf] dark:bg-stone-800 p-5 rounded-2xl shadow-md w-96 ">
         <h3 className="text-xl font-semibold">{message}</h3>
         <div className="flex justify-end gap-2 mt-4">
           <Button
             variant="outline"
-            className="text-gray-700"
+            className="dark:text-white"
             onClick={onCancel}
           >
             Cancel
           </Button>
           <Button
             variant="outline"
-            className="bg-red-500 text-white"
+            className="bg-red-500 dark:bg-red-500 text-white"
             onClick={onConfirm}
           >
             Confirm
